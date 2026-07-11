b17531384d05e382f478451e5b41fd0cea851627
Eaz1 <yeabruh652@gmail.com>
2026-07-10 23:57:55 -0700

    Refactor GalacticKillStreaks with manager initialization

--- src/main/java/me/eaz/galactickillstreaks/GalacticKillStreaks.java
+++ src/main/java/me/eaz/galactickillstreaks/GalacticKillStreaks.java
@@ -1,31 +1,81 @@
 package me.eaz.galactickillstreaks;
 
+import me.eaz.galactickillstreaks.data.DataManager;
+import me.eaz.galactickillstreaks.data.StorageManager;
+import me.eaz.galactickillstreaks.listeners.PlayerDeathListener;
+import me.eaz.galactickillstreaks.managers.AutoBountyManager;
+import me.eaz.galactickillstreaks.managers.KillStreakManager;
+import me.eaz.galactickillstreaks.managers.RewardManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GalacticKillStreaks extends JavaPlugin {
 
     private static GalacticKillStreaks instance;
 
+    private DataManager dataManager;
+    private StorageManager storageManager;
+    private KillStreakManager killStreakManager;
+    private RewardManager rewardManager;
+    private AutoBountyManager autoBountyManager;
+
     @Override
     public void onEnable() {
+
         instance = this;
 
-        // Create config if it doesn't exist
         saveDefaultConfig();
 
-        getLogger().info("=================================");
+        // Managers
+        dataManager = new DataManager();
+        storageManager = new StorageManager(this);
+        killStreakManager = new KillStreakManager();
+        rewardManager = new RewardManager(this);
+        autoBountyManager = new AutoBountyManager(this);
+
+        // Load saved player data
+        storageManager.loadPlayerData(dataManager);
+
+        // Register listeners
+        getServer().getPluginManager().registerEvents(
+                new PlayerDeathListener(this, killStreakManager),
+                this
+        );
+
+        getLogger().info("--------------------------------");
         getLogger().info("GalacticKillStreaks Enabled!");
         getLogger().info("Version: " + getDescription().getVersion());
-        getLogger().info("Made for GalacticPVP");
-        getLogger().info("=================================");
+        getLogger().info("--------------------------------");
     }
 
     @Override
     public void onDisable() {
+
+        storageManager.savePlayerData(dataManager);
+
         getLogger().info("GalacticKillStreaks Disabled!");
     }
 
     public static GalacticKillStreaks getInstance() {
         return instance;
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public StorageManager getStorageManager() {
+        return storageManager;
+    }
+
+    public KillStreakManager getKillStreakManager() {
+        return killStreakManager;
+    }
+
+    public RewardManager getRewardManager() {
+        return rewardManager;
+    }
+
+    public AutoBountyManager getAutoBountyManager() {
+        return autoBountyManager;
     }
 }
