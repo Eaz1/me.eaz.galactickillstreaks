958738aaeee0c6a7b20d0adb7e5ec218b63aacbd
Eaz1 <yeabruh652@gmail.com>
2026-07-11 00:23:25 -0700

    Refactor GalacticKillStreaks for better organization

--- src/main/java/me/eaz/galactickillstreaks/GalacticKillStreaks.java
+++ src/main/java/me/eaz/galactickillstreaks/GalacticKillStreaks.java
@@ -1,11 +1,17 @@
 package me.eaz.galactickillstreaks;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.eaz.galactickillstreaks.commands.GKSCommand;
+import me.eaz.galactickillstreaks.commands.GKSTabCompleter;
+import me.eaz.galactickillstreaks.commands.KillStreakCommand;
+import me.eaz.galactickillstreaks.commands.RecordCommand;
+import me.eaz.galactickillstreaks.commands.TopStreaksCommand;
 import me.eaz.galactickillstreaks.data.DataManager;
 import me.eaz.galactickillstreaks.data.StorageManager;
-import me.eaz.galactickillstreaks.listeners.PlayerDeathListener;
-import me.eaz.galactickillstreaks.managers.AutoBountyManager;
-import me.eaz.galactickillstreaks.managers.KillStreakManager;
-import me.eaz.galactickillstreaks.managers.RewardManager;
+import me.eaz.galactickillstreaks.listeners.*;
+import me.eaz.galactickillstreaks.managers.*;
+import me.eaz.galactickillstreaks.placeholders.KillStreakPlaceholderExpansion;
+import me.eaz.galactickillstreaks.utils.MessageUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GalacticKillStreaks extends JavaPlugin {
@@ -14,9 +20,14 @@ public class GalacticKillStreaks extends JavaPlugin {
 
     private DataManager dataManager;
     private StorageManager storageManager;
+    private ConfigManager configManager;
     private KillStreakManager killStreakManager;
     private RewardManager rewardManager;
     private AutoBountyManager autoBountyManager;
+    private RecordManager recordManager;
+    private BroadcastManager broadcastManager;
+    private CombatManager combatManager;
+    private RewardScheduler rewardScheduler;
 
     @Override
     public void onEnable() {
@@ -25,26 +36,48 @@ public class GalacticKillStreaks extends JavaPlugin {
 
         saveDefaultConfig();
 
-        // Managers
+        MessageUtil.initialize(this);
+
         dataManager = new DataManager();
         storageManager = new StorageManager(this);
+        configManager = new ConfigManager(this);
         killStreakManager = new KillStreakManager();
         rewardManager = new RewardManager(this);
         autoBountyManager = new AutoBountyManager(this);
+        recordManager = new RecordManager(this);
+        broadcastManager = new BroadcastManager(this);
+        combatManager = new CombatManager();
 
-        // Load saved player data
-        storageManager.loadPlayerData(dataManager);
-
-        // Register listeners
-        getServer().getPluginManager().registerEvents(
-                new PlayerDeathListener(this, killStreakManager),
-                this
+        rewardScheduler = new RewardScheduler(
+                this,
+                rewardManager,
+                autoBountyManager,
+                broadcastManager,
+                recordManager
         );
 
-        getLogger().info("--------------------------------");
-        getLogger().info("GalacticKillStreaks Enabled!");
-        getLogger().info("Version: " + getDescription().getVersion());
-        getLogger().info("--------------------------------");
+        storageManager.loadPlayerData(dataManager);
+
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this, killStreakManager), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerRespawnListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerCommandListener(this), this);
+        getServer().getPluginManager().registerEvents(new EntityDamageListener(this, combatManager), this);
+        getServer().getPluginManager().registerEvents(new BountyClaimListener(this), this);
+
+        getCommand("gks").setExecutor(new GKSCommand(this));
+        getCommand("gks").setTabCompleter(new GKSTabCompleter());
+
+        getCommand("killstreak").setExecutor(new KillStreakCommand(this));
+        getCommand("topstreaks").setExecutor(new TopStreaksCommand(this));
+        getCommand("record").setExecutor(new RecordCommand(this));
+
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new KillStreakPlaceholderExpansion(this).register();
+        }
+
+        getLogger().info("GalacticKillStreaks has been enabled!");
     }
 
     @Override
@@ -52,7 +85,7 @@ public class GalacticKillStreaks extends JavaPlugin {
 
         storageManager.savePlayerData(dataManager);
 
-        getLogger().info("GalacticKillStreaks Disabled!");
+        getLogger().info("GalacticKillStreaks has been disabled!");
     }
 
     public static GalacticKillStreaks getInstance() {
@@ -67,6 +100,10 @@ public class GalacticKillStreaks extends JavaPlugin {
         return storageManager;
     }
 
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
     public KillStreakManager getKillStreakManager() {
         return killStreakManager;
     }
@@ -77,5 +114,21 @@ public class GalacticKillStreaks extends JavaPlugin {
 
     public AutoBountyManager getAutoBountyManager() {
         return autoBountyManager;
+    }
+
+    public RecordManager getRecordManager() {
+        return recordManager;
+    }
+
+    public BroadcastManager getBroadcastManager() {
+        return broadcastManager;
+    }
+
+    public CombatManager getCombatManager() {
+        return combatManager;
+    }
+
+    public RewardScheduler getRewardScheduler() {
+        return rewardScheduler;
     }
 }
